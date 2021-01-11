2058c1f031220e88dc98ec9ca0a9c3da1b97a4a8
Ruben Castro Espinoza <rubencastroe@gmail.com>
2021-01-11 11:32:16 -0800

    modified application structure

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
-from flask import Flask, render_template
+from application import app
 
-
-
-app = Flask(__name__)
-
-
-@app.route('/')
-@app.route('/index')
-@app.route('/home')
-def home():
-    return "Hello World"
-    

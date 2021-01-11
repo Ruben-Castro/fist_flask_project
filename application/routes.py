4e6e039cc61c2b137a8938c0db4bd1282cce7bd3
Ruben Castro Espinoza <rubencastroe@gmail.com>
2021-01-11 15:38:49 -0800

    modified routes to handle data from the form via POST instead of GET. Modified Courses form action to POST instead of GET

--- application/routes.py
+++ application/routes.py
@@ -61,10 +61,10 @@ def register():
 def login():
     return render_template("login.html", login= True)
 
-@app.route("/enrollement")
+@app.route("/enrollement", methods=['GET', 'POST'])
 def enrollment():
-    id = request.args.get('courseID')
-    title= request.args.get('title')
-    term = request.args.get('term')
+    id = request.form.get('courseId')
+    title= request.form.get('title')
+    term = request.form.get('term')
 
     return render_template("enrollment.html", enrollment= True, data = {"id": id, "title": title, "term": term})

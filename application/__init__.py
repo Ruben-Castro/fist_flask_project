84bfcacbb21f4a96625fbb8fb1489ff01ac09dac
Ruben Castro Espinoza <rubencastroe@gmail.com>
2021-01-11 19:32:22 -0800

    modified init file to use base cofig added MongoDB to the app modified config file to connect to mongoDB

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
-
-
+from config import BaseConfig
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
+app.config.from_object(BaseConfig)
+
+db = MongoEngine()
+db.init_app(app)
 
 
 from application import routes
--- config.py
+++ config.py
@@ -2,6 +2,8 @@ import os
 
 class BaseConfig(object):
     SECRET_KY = os.environ.get('SECRET_KEY') or "secret_string"
-    
+    MONGODB_SETTINGS = {
+        'db': 'UTA_Enrollment'
+    }
 
 #class DevConfig(BaseConfig):

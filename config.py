edc7661c9589b4720ef0b452094eae036d86bc09
Ruben Castro Espinoza <rubencastroe@gmail.com>
2021-01-13 14:42:56 -0800

    added a temporary secret key to config file

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class BaseConfig(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(32)
+    SECRET_KEY = os.environ.get('SECRET_KEY') or b'\xabu9s\xb7(\x81Ht\xd8\x95\xd7\x1a\xf4\x1a\xcd\x10\xd0\xd7\xa7\xac\xa4ic\xf8A\x9c\x830jG\\'
     MONGODB_SETTINGS = {
         'db': 'UTA_Enrollment'
     }

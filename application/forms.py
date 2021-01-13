69e97a7ccd1fcf8d94d56eb88edd46d3ba4a5b75
Ruben Castro Espinoza <rubencastroe@gmail.com>
2021-01-13 12:18:54 -0800

    imported application models User

--- application/forms.py
+++ application/forms.py
@@ -1,13 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField,  SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+from application.models import User
+
 
 class RegisterForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
-    password = StringField("Password", validators=[DataRequired(), Length(min=6, max=20)])
-    confirm_password = StringField("Confirm Password", validators=[DataRequired(), Length(min=6, max=20),EqualTo('password')])
-    first_name = StringField("First Name", validators=[DataRequired(), Length(min=2, max=55)])
-    last_name = StringField("Last Name", validators=[DataRequired(), Length(min=2, max=55)])
+    password = StringField(
+        "Password", validators=[DataRequired(), Length(min=6, max=20)]
+    )
+    confirm_password = StringField(
+        "Confirm Password",
+        validators=[DataRequired(), Length(min=6, max=20), EqualTo("password")],
+    )
+    first_name = StringField(
+        "First Name", validators=[DataRequired(), Length(min=2, max=55)]
+    )
+    last_name = StringField(
+        "Last Name", validators=[DataRequired(), Length(min=2, max=55)]
+    )
     submit = SubmitField("Register")
 
     def validate_email(self, email):
@@ -18,6 +29,8 @@ class RegisterForm(FlaskForm):
 
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
-    password = StringField("Password", validators=[DataRequired(), Length(min=6, max=20)])
+    password = StringField(
+        "Password", validators=[DataRequired(), Length(min=6, max=20)]
+    )
     remeber_me = BooleanField("Remember Me")
     submit = SubmitField("Login")

45767efb5242f33bb28fe4f2417cea86131c4dbf
Ruben Castro Espinoza <rubencastroe@gmail.com>
2021-01-13 12:02:27 -0800

    modified registration and login forms to have password field

--- application/forms.py
+++ application/forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired
 
 class RegisterForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired()])
-    passowrd = StringField("Password", validators=[DataRequired()])
+    password = StringField("Password", validators=[DataRequired()])
     confirm_password = StringField("Confirm Password", validators=[DataRequired()])
     first_name = StringField("First Name", validators=[DataRequired()])
     last_name = StringField("Last Name", validators=[DataRequired()])
@@ -14,6 +14,6 @@ class RegisterForm(FlaskForm):
 
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired()])
-    passowrd = StringField("Password", validators=[DataRequired()])
+    password = StringField("Password", validators=[DataRequired()])
     remeber_me = BooleanField("Remember Me")
     submit = SubmitField("Login")

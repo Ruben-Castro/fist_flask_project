46fff463c54881243a10d03d088ef38a25ace5d3
Ruben Castro Espinoza <rubencastroe@gmail.com>
2021-01-13 17:24:46 -0800

    added flast_restplus to project

--- application/__init__.py
+++ application/__init__.py
@@ -1,17 +1,16 @@
 from flask import Flask
 from config import BaseConfig
 from flask_mongoengine import MongoEngine
+from flask_restplus import Api
 
+
+api = Api()
 app = Flask(__name__)
 app.config.from_object(BaseConfig)
 
 db = MongoEngine()
 db.init_app(app)
+api.init_app(app)
 
 
 from application import routes
-
-
-
-
-    
